b76e89abc4747e4bc7d5dbb6bb4592e565fa80de
Michail Gurbanov <54814200+reqww@users.noreply.github.com>
2020-06-13 13:40:23 +0300

    almost nothing

--- backend/account/api/views.py
+++ backend/account/api/views.py
@@ -1,8 +1,9 @@
 from account.api.serializers import RegistrationSerializer
 from account.models import Account
+from rest_framework import generics
 
 class RegistrationView(generics.CreateAPIView):
 
     queryset = Account.objects.all()
-    serializer_class = RegistrationSerializer
-
+    serializer_class =  RegistrationSerializer
+    
