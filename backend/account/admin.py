23c1571193ff4d136a3643efcc95568426e9f393
Michail Gurbanov <54814200+reqww@users.noreply.github.com>
2020-06-14 13:55:29 +0300

    suka

--- backend/account/admin.py
+++ backend/account/admin.py
@@ -6,9 +6,6 @@ class ProductAdmin(admin.ModelAdmin):
 	list_display = ('email', 'username', 'date_joined', 'last_login', 'is_admin')
 
 @admin.register(MyToken)
-class MyTokenAdmin(admin.ModelAdmin):
-<<<<<<< HEAD
+class MytokenAdmin(admin.ModelAdmin):
 	list_display = ('user', 'token', 'created')
-=======
-	list_display = ('user', 'token', 'created', 'is_used')
->>>>>>> 8355c4110e54323cc74ff7e5a2cababe86a887b9
+
