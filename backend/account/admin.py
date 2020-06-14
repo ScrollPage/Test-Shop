a9b42e663b8d3d1cb6a72accf58b9d98514c391e
Michail Gurbanov <54814200+reqww@users.noreply.github.com>
2020-06-14 14:53:54 +0300

    not a cart
    rebuild account model

--- backend/account/admin.py
+++ backend/account/admin.py
@@ -3,7 +3,7 @@ from account.models import Account, MyToken
 
 @admin.register(Account)
 class ProductAdmin(admin.ModelAdmin):
-	list_display = ('email', 'username', 'date_joined', 'last_login', 'is_admin')
+	list_display = ('email', 'first_name', 'last_name', 'phone_number', 'date_joined', 'last_login', 'is_admin')
 
 @admin.register(MyToken)
 class MytokenAdmin(admin.ModelAdmin):
