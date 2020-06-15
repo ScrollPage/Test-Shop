6d70764cd4b7632abb4ffde2da592bb509eb9000
unknown <0r20@mail.ru>
2020-06-15 23:45:34 +0300

    basket

--- frontend/src/components/BasketCart.js
+++ frontend/src/components/BasketCart.js
@@ -1,28 +1,29 @@
-import React, { useContext, useEffect } from 'react'
-import { Link } from 'react-router-dom'
-import store from 'store'
-import { BasketContext } from '../context/basket/BasketContext'
+import React from 'react'
+// import { Link } from 'react-router-dom'
+// import store from 'store'
+// import { BasketContext } from '../context/basket/BasketContext'
 
 
 export const BasketCart = () => {
 
-    const { basket } = useContext(BasketContext)
+    // const { basket } = useContext(BasketContext)
 
-    useEffect(() => {
-        store.set('basket', basket)
-    }, [basket])
+    // useEffect(() => {
+    //     store.set('basket', basket)
+    // }, [basket])
 
-    const basketSum = (basket) => {
-        let newBasket = 0
-        basket.forEach((item) => {
-            newBasket = newBasket + item.price
-        })
-        return newBasket
-    }
+    // const basketSum = (basket) => {
+    //     let newBasket = 0
+    //     basket.forEach((item) => {
+    //         newBasket = newBasket + item.price
+    //     })
+    //     return newBasket
+    // }
 
     return (
-        <Link to="/basket" className="btn btn-lg btn-block btn-outline-primary">
-            {basket.length} товаров - {basketSum(basket)}&nbsp;₽
-        </Link>
+        // <Link to="/basket" className="btn btn-lg btn-block btn-outline-primary">
+        //     {basket.length} товаров - {basketSum(basket)}&nbsp;₽
+        // </Link>
+        <h4>Будущая корзина</h4>
     )
 }
--- frontend/src/containers/Basket.js
+++ frontend/src/containers/Basket.js
@@ -1,13 +1,18 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect } from 'react'
 // import { BasketContext } from '../context/basket/BasketContext'
 import useReactRouter from 'use-react-router'
 import { Button } from 'antd'
 import { BasketItem } from '../components/BasketItem' 
 import { BasketContext } from '../context/basket/BasketContext'
+import { Loading, Loader } from '../components/Loader'
 
 export const Basket = () => {
 
-    const { clearItemToBasket } = useContext(BasketContext)
+    useEffect(() => {
+        fetchBasket()
+    }, [])
+
+    const { clearItemToBasket, fetchBasket, basket, loading } = useContext(BasketContext)
     const { history } = useReactRouter()
 
     const renderSidebar = () => (
@@ -21,36 +26,7 @@ export const Basket = () => {
         </div>
     )
 
-    const data = [
-        {
-            key: 1,
-            name: 'Apple IPhone 10',
-            price: '1200$',
-            amount: '1',
-            description: 'Далеко-далеко за словесными горами в стране гласных и согласных живут рыбные тексты. Первую страна эта это проектах пустился, вскоре вершину осталось семантика себя предложения переписывается? Переулка, взобравшись.',
-        },
-        {
-            key: 2,
-            name: 'Apple IPhone 10',
-            price: '1200$',
-            amount: '1',
-            description: 'Далеко-далеко за словесными горами в стране гласных и согласных живут рыбные тексты. Первую страна эта это проектах пустился, вскоре вершину осталось семантика себя предложения переписывается? Переулка, взобравшись.',
-        },
-        {
-            key: 3,
-            name: 'Apple IPhone 10',
-            price: '1200$',
-            amount: '1',
-            description: 'Далеко-далеко за словесными горами в стране гласных и согласных живут рыбные тексты. Первую страна эта это проектах пустился, вскоре вершину осталось семантика себя предложения переписывается? Переулка, взобравшись.',
-        },
-        {
-            key: 4,
-            name: 'Apple IPhone 10',
-            price: '1200$',
-            amount: '1',
-            description: 'Далеко-далеко за словесными горами в стране гласных и согласных живут рыбные тексты. Первую страна эта это проектах пустился, вскоре вершину осталось семантика себя предложения переписывается? Переулка, взобравшись.',
-        }
-    ];
+    // const data = basket.items;
 
     return (
         <div className="basket">
@@ -60,10 +36,15 @@ export const Basket = () => {
                         <div className="basket-items">
                             <div className="basket-title">
                                 <h4>4 ТОВАРА В КОРЗИНЕ</h4>
+                                {loading
+                                ? <Loader />
+                                : basket.length === 0
+                                ? <p>Корзина пуста</p>
+                                : basket.map(item => (
+                                    <BasketItem key={item.id} data={item}/>
+                                ))}
                             </div>
-                            {data.map(item => (
-                                <BasketItem key={item.key+item.name} data={item}/>
-                            ))}
+                            {}
                         </div>
                     </div>
                     <div className="col-md-3 mb-4">
