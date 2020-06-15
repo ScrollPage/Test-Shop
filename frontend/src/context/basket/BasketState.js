23bac0d5e9028e96e507479db8015938cf615712
unknown <0r20@mail.ru>
2020-06-15 14:56:45 +0300

    jquerry ajax

--- frontend/src/context/basket/BasketState.js
+++ frontend/src/context/basket/BasketState.js
@@ -1,34 +1,71 @@
-import React, {useReducer} from 'react'
+import React, { useReducer } from 'react'
 import PropTypes from 'prop-types'
-import {ADD_ITEM_TO_BASKET} from '../types'
-import {BasketContext} from './BasketContext'
-import {BasketReducer} from './BasketReducer'
+import { ADD_ITEM_TO_BASKET } from '../types'
+import { BasketContext } from './BasketContext'
+import { BasketReducer } from './BasketReducer'
 import store from 'store'
-import axios from 'axios'
+// import axios from 'axios'
+import $ from 'jquery'
+export const BasketState = ({ children }) => {
 
-export const BasketState = ({children}) => {
-
-    const [state, dispatch] = useReducer(BasketReducer, 
+    const [state, dispatch] = useReducer(BasketReducer,
         store.get('store') === undefined ? [] : store.get('store')
     )
 
+    function getCookie(name) {
+        var cookieValue = null;
+        if (document.cookie && document.cookie !== '') {
+            var cookies = document.cookie.split(';');
+            for (var i = 0; i < cookies.length; i++) {
+                var cookie = cookies[i].trim();
+                if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                    cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                    break;
+                }
+            }
+        }
+        return cookieValue;
+    }
+
     const addItemToBasket = (item) => {
-        dispatch({type: ADD_ITEM_TO_BASKET, payload: item})
+        dispatch({ type: ADD_ITEM_TO_BASKET, payload: item })
         console.log(item.id, store.get('email'))
-        axios.defaults.xsrfCookieName = 'csrftoken'
-        axios.defaults.xsrfHeaderName = 'X-CSRFToken'
-        axios.post("http://localhost:8000/cart/add", {
-            uid: item.id, amount: 1, email: store.get('email')
-        })
-            .then((response) => {
-                console.log(response.data)
-            })
-            .catch((error) => {
-                console.log(error);
+
+        // const options = {
+        //     method: 'POST',
+        //     url: "http://localhost:8000/cart/add",
+        //     data: { uid: item.id, amount: 1, email: store.get('email') },
+        //     // headers: { 'xsrfCookieName': 'csrftoken', 'xsrfHeaderName': 'X-CSRFToken'},
+        //     // withCredentials: true
+        // }
+
+        // axios(options)
+        //     .then((response) => {
+        //         console.log(response.data)
+        //     })
+        //     .catch((error) => {
+        //         console.log(error);
+        //     });
+
+        
+            $.ajax({
+                type: "POST",
+                url: "http://localhost:8000/cart/add",
+                data: {
+                    csrfmiddlewaretoken: getCookie('csrftoken'),
+                    uid: item.id, amount: 1, email: store.get('email') 
+                },
+                dataType: "text",
+
+                success: function (data) {
+                    if (data) {
+                        console.log(data)
+                    }
+                }
             });
     }
 
-    return(
+    return (
         <BasketContext.Provider value={{
             addItemToBasket, basket: state
         }}>
