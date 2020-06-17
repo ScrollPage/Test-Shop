5a3a4efeaaefc6a2a12fdde585282894a48dd2fc
Michail Gurbanov <54814200+reqww@users.noreply.github.com>
2020-06-17 19:44:44 +0300

    undefined order

--- backend/cart/api/views.py
+++ backend/cart/api/views.py
@@ -9,10 +9,19 @@ class OrderView(generics.ListAPIView):
 
     def get_queryset(self):
         email = self.kwargs['email']
-        u = Account.objects.get(email = email)
-        o = Order.objects.filter(owner = u).all()
-        print(o)
-        queryset = o
+        if email == 'undefined':
+            queryset = []
+            data = {}
+            data['total_price'] = 0
+            data['total_count'] = 0
+            data['items'] = []
+            data['ref_code'] = None
+            queryset.append(data)
+        else:
+            u = Account.objects.get(email = email)
+            o = Order.objects.filter(owner = u).all()
+            print(o)
+            queryset = o
         return queryset
 
 
