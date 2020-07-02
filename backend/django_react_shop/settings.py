5f32ab48b3463171970113693d4da28245ba8602
Michail Gurbanov <54814200+reqww@users.noreply.github.com>
2020-07-02 12:45:02 +0300

    123

--- backend/django_react_shop/settings.py
+++ backend/django_react_shop/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+import local
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deploymen..
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'cvx&ylb92=6se=4=cbf=l53hodgscfo@g&bi(7yy9fy+vgb!yk'
+SECRET_KEY = local.SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
